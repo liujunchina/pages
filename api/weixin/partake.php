fcd59c536f6249d468bf7b538995aa4a52792253
liujunchina <asdf123456>
2016-10-13 12:33:53 +0800

    更新测试php

--- api/weixin/partake.php
+++ api/weixin/partake.php
@@ -1,5 +1,5 @@
 <?php
-	include "../WeixinJsSdk.class.php";
+	include "../class/WeixinJsSdk.class.php";
 	$jssdk = new JSSDK('wx02ac9ee3fc9854fc', '9e61b9e28a631042bdda3706ae1db053 ');
 	$signPackage = $jssdk->GetSignPackage();
 
